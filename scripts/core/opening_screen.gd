99775e526c2828e077516648e6275ad95b7cecf9
Mingrulu <a56165615@gmail.com>
2026-06-27 20:35:43 +0800

    add opening

--- scripts/core/opening_screen.gd
+++ scripts/core/opening_screen.gd
@@ -3,5 +3,6 @@ extends Control
 signal continue_pressed
 
 func _ready() -> void:
-	$BookPanel/ContentContainer/VBoxContainer/ContinueButton.pressed.connect(func() -> void: continue_pressed.emit())
-
+	$BookPanel/OuterMargin/Layout/ContinueButton.pressed.connect(
+		func() -> void: continue_pressed.emit()
+	)
